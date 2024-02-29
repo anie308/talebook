a99d2fc9a2a7dc8c65094cf9681a758d5ce4b661
Aniebietabasi <aniebietabasiobo@gmail.com>
2024-03-01 00:32:58 +0100

    api url change

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 
-// const PROD = 'https://47bf-197-210-85-137.ngrok-free.app/api/v1';
-const PROD = 'https://socialmedia-api-svgo.onrender.com/api/v1'
+const PROD = 'https://talebookapi.vercel.app/api/v1'
 export const apiSlice = createApi({
     reducerPath: 'apiSlice',
     // tagTypes: ['wallets', "bills", 'vaults'],
